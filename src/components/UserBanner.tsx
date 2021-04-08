47ea3b4a5fa9f06c459bafbc0975c91b05d76fa4
boojack <imrealleonardo@gmail.com>
2021-04-08 21:48:36 +0800

    -polish user banner;

--- src/components/UserBanner.tsx
+++ src/components/UserBanner.tsx
@@ -1,16 +1,41 @@
 import React from "react";
+import StateManager from "../helpers/StateManager";
 import "../less/user-banner.less";
 
 interface Props {
   userinfo: UserType;
 }
 
+interface State {
+  createdDays: number;
+  memosAmount: number;
+}
+
 export class UserBanner extends React.Component<Props> {
+  public state: State;
+
   constructor(props: Props) {
     super(props);
+
+    const { userinfo } = this.props;
+
+    this.state = {
+      createdDays: Math.ceil((Date.now() - new Date(userinfo.createdAt).getTime()) / 1000 / 3600 / 24),
+      memosAmount: (StateManager.getState("memos") as []).length || 0,
+    };
   }
 
-  public componentDidMount() {}
+  public componentDidMount() {
+    StateManager.bindStateChange("memos", this, (memos: MemoType[]) => {
+      this.setState({
+        memosAmount: memos.length,
+      });
+    });
+  }
+
+  public componentWillUnmount() {
+    StateManager.unbindStateListener("memos", this);
+  }
 
   public render() {
     const { userinfo } = this.props;
@@ -20,8 +45,12 @@ export class UserBanner extends React.Component<Props> {
         <div className="userinfo-container">
           <p className="username-text">{userinfo.username}</p>
           <div className="status-text-container">
-            <p className="memos-text">Memo: 5</p>
-            <p className="duration-text">Day: 100</p>
+            <p className="status-text memos-text">
+              memo: <span className="amount-text">{this.state.memosAmount}</span>
+            </p>
+            <p className="status-text duration-text">
+              day: <span className="amount-text">{this.state.createdDays}</span>
+            </p>
           </div>
         </div>
       </div>
