2b92591e4563917ee46cf053270b03d9ede852d0
boojack <imrealleonardo@gmail.com>
2021-04-09 11:15:10 +0800

    -fix api base_url;

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = window.location.host.indexOf(":3000") > -1 ? "http://localhost:8080" : "/";
+const BASE_URL = window.location.host.indexOf(":3000") > -1 ? "http://localhost:8080" : "";
 
 /**
  * api
