c558fbae34897ce8a7b703f9408b80f663832ee5
boojack <imrealleonardo@gmail.com>
2021-05-27 21:52:48 +0800

    -fix sw.js;

--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,17 @@
 self.addEventListener("install", function (e) {
-  e
-    .waitUntil
-    // caches.open("fox-store").then(function (cache) {
-    //   return cache.addAll(["/"]);
-    // })
-    ();
+  // do nth
+  // e.waitUntil(
+  //   caches.open("fox-store").then(function (cache) {
+  //     return cache.addAll(["/"]);
+  //   })
+  // );
 });
 
 self.addEventListener("fetch", function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (response) {
-      return response || fetch(e.request);
-    })
-  );
+  // do nth
+  // e.respondWith(
+  //   caches.match(e.request).then(function (response) {
+  //     return response || fetch(e.request);
+  //   })
+  // );
 });
