64838b4cab43491f3628f89d797e68014dff30bf
boojack <imrealleonardo@gmail.com>
2021-05-28 18:33:32 +0800

    -fix wx config;

--- public/wxconfig.js
+++ public/wxconfig.js
@@ -3,7 +3,7 @@ async function init() {
   const data = await rawRes.json();
 
   wx.config({
-    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+    debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
     appId: "wx7ee301c37cacda68", // 必填，公众号的唯一标识
     nonceStr: "Wm3WZYTPz0wzccnW", // 必填，生成签名的随机串
     timestamp: data.timestamp, // 必填，生成签名的时间戳
@@ -15,9 +15,8 @@ async function init() {
     wx.updateAppMessageShareData({
       title: "Memos", // 分享标题
       desc: "💡 Have an idea? write in here!", // 分享描述
-      link: "https://insmemo.justsven.top", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+      link: "https://insmemo.justsven.top/", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
       imgUrl: "https://insmemo.justsven.top/logo-fill.png", // 分享图标
-      type: "link", // 分享类型,music、video或link，不填默认为link
       success: function () {
         // do nth
       },
