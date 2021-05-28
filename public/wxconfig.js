595c40ff2d61baf4a5f0a20a927ad2542b343f13
boojack <imrealleonardo@gmail.com>
2021-05-28 16:52:16 +0800

    -fix fetch handler;

--- public/wxconfig.js
+++ public/wxconfig.js
@@ -1,5 +1,6 @@
 async function init() {
-  const { data } = await fetch("/api/wx/signs");
+  const rawRes = await fetch("/api/wx/signs");
+  const data = await rawRes.json();
 
   wx.config({
     debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
