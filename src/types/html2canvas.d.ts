0c9a28450b62352e98aaf7806a4fd0c47e79470c
LeeShuang <sli4@ebay.com>
2021-09-23 12:10:14 +0800

    -update html2canvas;

--- src/types/html2canvas.d.ts
+++ src/types/html2canvas.d.ts
@@ -1,11 +1,9 @@
-// Type definitions for html2canvas.js 1.0-alpha
 // Project: https://github.com/niklasvh/html2canvas
 // Definitions by: Richard Hepburn <https://github.com/rwhepburn>
 //                 Pei-Tang Huang <https://github.com/tan9>
 //                 Sebastian Schocke <https://github.com/sschocke>
 //                 Rickard Staaf <https://github.com/Ristaaf>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
 
 interface Html2CanvasOptions {
   /** Whether to parse and render the element asynchronously */
