e9c4b410878710d35eddb61c140ab4461a08f55a
liuzhengjun <lzj12530@sina.com>
2021-12-07 17:05:29 +0800

    引入Prismjs做代码高亮

--- src/helpers/marked.ts
+++ src/helpers/marked.ts
@@ -5,6 +5,7 @@
  * - 加粗/斜体；
  * - TODO;
  */
+import Prism from 'prismjs';
 const CODE_BLOCK_REG = /```([\s\S]*?)```/g;
 const BOLD_TEXT_REG = /\*\*(.+?)\*\*/g;
 const EM_TEXT_REG = /\*(.+?)\*/g;
@@ -13,9 +14,27 @@ const DONE_BLOCK_REG = /\[x\] /g;
 const DOT_LI_REG = /[*] /g;
 const NUM_LI_REG = /(\d+)\. /g;
 
+const parseCodeToPrism = (codeStr: string) : string => {
+  const getLang = (str:string): string => {
+    let lang = /^\w+/g.exec(str)
+    if (lang !== null) {
+      return lang[0]
+    }
+    return 'javascript'
+  }
+  return codeStr.replace(CODE_BLOCK_REG, (match, p1): string => {
+    let lang = getLang(p1);
+    let s = p1.replace(lang, '').replace(/<p>/g,'').replace(/<\/p>/g,'\r\n').replace(/<br>/g,'\r\n').replace(/&nbsp;/g,' ');
+    // 特定语言处理
+    switch(lang) {
+      case 'html': s = s.replace(/&lt;/g,'<').replace(/&gt;/g,'>')
+    }
+    let str = Prism.highlight(s, Prism.languages[lang], lang)
+    return `<pre>${str}</pre>`
+  })
+}
 const parseMarkedToHtml = (markedStr: string): string => {
-  const htmlText = markedStr
-    .replace(CODE_BLOCK_REG, "<pre lang=''>$1</pre>")
+  const htmlText = parseCodeToPrism(markedStr)
     .replace(DOT_LI_REG, "<span class='counter-block'>•</span>")
     .replace(NUM_LI_REG, "<span class='counter-block'>$1.</span>")
     .replace(TODO_BLOCK_REG, "<span class='todo-block' data-type='todo'>⬜</span>")
